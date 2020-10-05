4f60832f9d587a6e26a1df98164a9d1470d80fac
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2020-10-05 03:14:16 -0300

    Saitoso quase prontoso

--- aboutUpChangeUp.js
+++ aboutUpChangeUp.js
@@ -35,3 +35,41 @@ $(document).ready(function (){
         $('#aboutUsText').text("Artist").stop(true,true).hide().fadeIn(1000, "swing");
     });
 })
+
+$(document).ready(function (){
+    $('#capudinoPhotoPhone').click(function () {
+        $('#aboutUsPhotoPhone > img').attr("src", "./images/CapudinoLogo.png").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTitlePhone').text("Capudino").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTextPhone').text('Capudino Studios was founded in 2018.\nWe are an independent game developer and publisher studio, based in Brasília, Brazil, formed by five friends and college colleagues that share a love for pixel art and 3D toon art.\nOur goal is to create games that can be played and enjoyed by all, without giving up of creating a healthy studio and a respectful community.').stop(true,true).hide().fadeIn(1000, 'swing');
+    });
+
+    $('#rofoPhotoPhone').click(function () {
+        $('#aboutUsPhotoPhone > img').attr("src", "./images/RofoRofosoTotoso.jpg").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTitlePhone').text("Rafael Oliveira").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTextPhone').text("Programmer").stop(true,true).hide().fadeIn(1000, "swing");
+    });
+
+    $('#topoPhotoPhone').click(function () {
+        $('#aboutUsPhotoPhone > img').attr("src", "./images/TopoToposo.jpg").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTitlePhone').text("Pedro Tepedino").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTextPhone').text("Lead Programmer").stop(true,true).hide().fadeIn(1000, "swing");
+    });
+
+    $('#dovoPhotoPhone').click(function () {
+        $('#aboutUsPhotoPhone > img').attr("src", "./images/DavizeraRera.jpg").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTitlePhone').text("Davi Carrijo").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTextPhone').text("VFX Artist").stop(true,true).hide().fadeIn(1000, "swing");
+    });
+
+    $('#joooPhotoPhone').click(function () {
+        $('#aboutUsPhotoPhone > img').attr("src", "./images/JoooOoo.jpg").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTitlePhone').text("João de Lorenzi").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTextPhone').text("Artist").stop(true,true).hide().fadeIn(1000, "swing");
+    });
+
+    $('#josoPhotoPhone').click(function () {
+        $('#aboutUsPhotoPhone > img').attr("src", "./images/JosoJososo.jpg").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTitlePhone').text("José Karnikowski").stop(true,true).hide().fadeIn(1000, "swing");
+        $('#aboutUsTextPhone').text("Artist").stop(true,true).hide().fadeIn(1000, "swing");
+    });
+})
